135a0cce0a3b8b7852a863467818a680b2f940cd
mismetdemir <mismetdemir@hotmail.com>
2026-05-07 15:55:25 +0300

    Added game states

--- js/game.js
+++ js/game.js
@@ -2,6 +2,7 @@ import { keys } from "./input.js";
 import { createPlayer, updatePlayer, drawPlayer } from "./player.js";
 import { enemies, resetEnemies, updateEnemySpawn, updateEnemies, drawEnemies } from "./enemy.js";
 
+
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
@@ -10,8 +11,52 @@ canvas.height = window.innerHeight;
 
 const player = createPlayer(canvas);
 
+const GAME_STATE = {
+    START: "start",
+    PLAYING: "playing",
+    LEVEL_UP: "levelUp",
+    PAUSED: "paused",
+    GAME_OVER: "gameOver"
+}
+
+let currentState = GAME_STATE.START;
+let lastTime = performance.now();
+let player = createPlayer(canvas);
+
 const game = {
     elapsedTime: 0
+}
+
+canvas.addEventListener("click", (event) => {
+    if (currentState === GAME_STATE.START) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const clickX = event.clientX - rect.left;
+    const clickY = event.clientY - rect.top;
+    const buttons = getStartButtons(canvas);
+
+    for (let i = 0; i < buttons.length; i++) {
+        const button = buttons[i];
+        const isInside = clickX >= button.x &&
+                        clickX <= button.x + button.width &&
+                        clickY >= button.y &&
+                        clickY <= button.y + button.height;
+        
+        if (isInside && button.id === "classic") {
+            startGame();
+        }
+    }
+})
+
+function startGame() {
+    resetGame();
+    currentState = GAME_STATE.PLAYING;
+}
+
+function resetGame() {{
+    player = createPlayer(canvas);
+    resetEnemies();
+    game.elapsedTime = 0;
 }
 
 function update(deltaTime) {
@@ -28,18 +73,32 @@ function drawGame() {
 
     ctx.fillStyle = "limegreen";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     drawPlayer(ctx, player);
     drawEnemies(ctx);
 }
 
-let lastTime = performance.now();
-
 function gameLoop(currentTime) {
-    const deltaTime = (currentTime - lastTime) / 1000;
+    let deltaTime = (currentTime - lastTime) / 1000;
     lastTime = currentTime;
 
-    update(deltaTime);
-    drawGame();
+    if (deltaTime > 0.05) deltaTime = 0.05;
+
+    if (currentState === GAME_STATE.START) {
+        drawStartScreen(ctx, canvas);
+    } else if (currentState === GAME_STATE.PLAYING) {
+        update(deltaTime);
+        drawGame();
+    } else if (currentState === GAME_STATE.PAUSED) {
+        drawGame();
+        drawPauseScreen(ctx, canvas, player);
+    } else if (currentState === GAME_STATE.LEVEL_UP) {
+        drawGame();
+        drawLevelUpScreen(ctx, canvas, player);
+    } else if (currentState === GAME_STATE.GAME_OVER) {
+        drawGame();
+        drawGameOverScreen(ctx, canvas, game.elapsedTime);
+    }
 
     requestAnimationFrame(gameLoop);
 }
